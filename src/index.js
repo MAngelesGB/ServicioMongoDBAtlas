a3397f481377e31c3087647947a83676b1080607
MAngelesGB <mgutierrez6@ucol.mx>
2023-04-18 22:15:30 -0600

    conexionMongoDBComentarios

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-const express = require('express'); //se inyecta el modulo de express
-const mongoose = require('mongoose'); // se inyecta el modulo mongoose
-require('dotenv').config(); // se inyecta el modulo dotenv para customizar variables de ambiente
+const express = require('express'); //se importa la dependencia de express
+const mongoose = require('mongoose'); // se importa la dependencia de mongoose
+/* se importa la dependencia dotenv y se ejecuta con el .config() 
+para accessar a variables de ambiente que hayamos creado*/
+require('dotenv').config(); 
 
 const app = express(); // se ejecuta express
-const port = process.env.PORT || 9000; // puerto por el que se escucha, se le pase una variable de ambiente, es decir toma un puerto que se da por default, en caso de no estar disponible toma el que se asigno
+/*se le pasa una variable de ambiente de node.js. Si se despliega la aplicacion en un servicio de hosting
+permite tomar un puerto que da el servicio, y en caso de no estar disponible toma el que se le dio previamente*/ 
+const port = process.env.PORT || 9000; // puerto por el que se va a escuchar. 
 
-//routes
-//ruta que muestra un mensaje
+//routes --------------------------------------------
+
+//ruta definida para verificar que el servidor este respondiendo correctamente
 app.get('/', (req, res) =>{
-    res.send("Welcome to my API"); 
+    res.send("Welcome to my API"); //mensaje a mostrar
 });
 
-//variable de ambiente para customizar la ruta para la conexion a la bd
+//mongdb connection --------------------------------
 
-// en este apartado se contecta la bd con mongoose 
-//Una vez establecido la variable de ambiente con dotenv para la conexion a la base de datos, se pasa a m. 
+/*
+
+--> Para la conexion se necesita de una URI que es una llave que proporciona la misma plataforma de mongodb atlas 
+para obtener acceso a la bd creada en esa misma plataforma. 
+
+--> Por medio de dotenv se crea una variable de ambiente customizada. Se crea un nuevo archivo donde se crea la variable
+MONGODB_URI en donde se le asigna la llave para el acceso a la bd.  
+
+--|variable de ambiente: son cadenas con informacion del entorno del sistema y del usuario que haya iniciado sesion
+
+--> Se usa la variable que se creo para la conexion.
+    -- Se muestra un mensaje en caso de que la conexion sea exitosa. 
+    -- se utiliza un catch para mostrar el errror en caso de que ocurra alguno.     
+*/
 mongoose.connect(process.env.MONGODB_URI).then(()=> console.log("Connected to MongoDB Atlas")).catch((error)=>console.error(error));
 
 app.listen(port, () => console.log('server listening on port', port)) // puerto a la escucha 
